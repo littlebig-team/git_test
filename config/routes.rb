ed3b4ed5e8ee491080f81418cc65f0537c609975
littlebig-team <herstorycoach@likelion.org>
2019-11-23 06:57:15 +0000

    added second page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'homes/index'
   root 'homes#index'
+  get 'homes/second'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
